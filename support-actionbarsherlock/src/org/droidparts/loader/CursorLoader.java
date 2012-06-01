5f3bb7a2e7f957466ffc69b85d3e60967ea91bb1
Alex Yanchenko <alex@yanchenko.com>
2012-06-02 01:14:59 +0300

    CursorLoader

---
+++ support-actionbarsherlock/src/org/droidparts/loader/CursorLoader.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2012 Alex Yanchenko
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.droidparts.loader;
+
+import org.droidparts.inject.Injector;
+
+import android.content.Context;
+import android.net.Uri;
+
+public abstract class CursorLoader extends
+		android.support.v4.content.CursorLoader {
+
+	public CursorLoader(Context ctx) {
+		super(ctx);
+		Injector.get().inject(ctx, this);
+	}
+
+	public CursorLoader(Context ctx, Uri uri, String[] projection,
+			String selection, String[] selectionArgs, String sortOrder) {
+		super(ctx, uri, projection, selection, selectionArgs, sortOrder);
+		Injector.get().inject(ctx, this);
+	}
+
+}
