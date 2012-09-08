d8350669b853078328220430fba83b1b96c8246a
Alex Yanchenko <alex@yanchenko.com>
2012-09-08 03:27:38 +0300

    Added license info.

--- sample/modern/src/org/droidparts/gram/persist/PrefsManager.java
+++ sample/modern/src/org/droidparts/gram/persist/PrefsManager.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2012 Alex Yanchenko
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 package org.droidparts.gram.persist;
 
 import java.util.Arrays;
@@ -18,11 +33,13 @@ public class PrefsManager extends AbstractPrefsManager {
 	}
 
 	public boolean isShowDetailFilter() {
-		return getShowDetailVaules().contains(ctx.getString(R.string.pref_filter));
+		return getShowDetailVaules().contains(
+				ctx.getString(R.string.pref_filter));
 	}
 
 	public boolean isShowDetailTags() {
-		return getShowDetailVaules().contains(ctx.getString(R.string.pref_tags));
+		return getShowDetailVaules()
+				.contains(ctx.getString(R.string.pref_tags));
 	}
 
 	private HashSet<String> getShowDetailVaules() {
