9369ff9117af4ff4dc21bd6bf4defbf0e8bd0868
Alex Yanchenko <alex@yanchenko.com>
2012-09-16 16:55:00 +0300

    Test case for getDependency().

--- test/src/org/droidparts/test/testcase/TestActivityTestCase.java
+++ test/src/org/droidparts/test/testcase/TestActivityTestCase.java
@@ -1,9 +1,11 @@
 package org.droidparts.test.testcase;
 
+import org.droidparts.inject.Injector;
 import org.droidparts.test.R;
 import org.droidparts.test.activity.TestActivity;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class TestActivityTestCase extends
@@ -21,6 +23,14 @@ public class TestActivityTestCase extends
 		assertEquals(testString, activity.testString);
 		assertNotNull(activity.textView);
 		assertEquals(testString, activity.textView.getText());
+	}
+
+	public void testInjection2() {
+		Class<SQLiteDatabase> cls = SQLiteDatabase.class;
+		SQLiteDatabase dependency = Injector.get().getDependency(getActivity(),
+				cls);
+		assertNotNull(dependency);
+		assertTrue(cls.isAssignableFrom(dependency.getClass()));
 	}
 
 }
