b2d193a274dc10dcb858c9c37c5f687c766fefd4
Alex Yanchenko <alex@yanchenko.com>
2012-10-09 16:29:09 +0300

    Added droidparts-modern-native.

---
+++ modern-native/src/org/droidparts/activity/SingleFragmentActivity.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2012 Alex Yanchenko
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.droidparts.activity;
+
+import org.droidparts.R;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+public abstract class SingleFragmentActivity<F extends Fragment> extends
+		FragmentActivity {
+
+	private F fragment;
+
+	@Override
+	public void onPreInject() {
+		setContentView(R.layout.activity_single_fragment);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		fragment = onCreateFragment();
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		ft.add(R.id.view_content, fragment);
+		ft.commit();
+	}
+
+	protected F getFragment() {
+		return fragment;
+	}
+
+	protected abstract F onCreateFragment();
+
+}
---
+++ modern-native/src/org/droidparts/fragment/DialogFragment.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2012 Alex Yanchenko
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.droidparts.fragment;
+
+import org.droidparts.activity.FragmentActivity;
+import org.droidparts.inject.FragmentsInjector;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogFragment extends android.app.DialogFragment {
+
+	@Override
+	public final View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		View view = onCreateView(savedInstanceState, inflater, container);
+		FragmentsInjector.get().inject(view, this);
+		return view;
+	}
+
+	public View onCreateView(Bundle savedInstanceState,
+			LayoutInflater inflater, ViewGroup container) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	public void show(FragmentActivity activity) {
+		String tag = getClass().getName();
+		FragmentManager fm = activity.getFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		Fragment f = fm.findFragmentByTag(tag);
+		if (f != null) {
+			ft.remove(f);
+		}
+		show(ft, tag);
+	}
+
+}
