a9c4c3e52d6d62537ff1bdd0c404d24df79dd858
Alex Yanchenko <alex@yanchenko.com>
2013-01-07 23:30:41 +0200

    ViewUtils: obsolete methods removed.

--- extra/src/org/droidparts/util/ui/ViewUtils.java
+++ extra/src/org/droidparts/util/ui/ViewUtils.java
@@ -19,17 +19,9 @@ import static android.content.Context.INPUT_METHOD_SERVICE;
 import static android.view.View.GONE;
 import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
-
-import java.lang.reflect.Method;
-
-import org.droidparts.adapter.ui.AnimationListenerAdapter;
-import org.droidparts.util.L;
-
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -47,25 +39,8 @@ public class ViewUtils {
 		}
 	}
 
-	public static void crossFade(final View visibleViewFrom,
-			final View invisibleViewTo, int durationMillis,
-			final Runnable onAnimationEnd) {
-		Animation animFrom = new AlphaAnimation(1, 0);
-		Animation animTo = new AlphaAnimation(0, 1);
-		animFrom.setDuration(durationMillis);
-		animTo.setDuration(durationMillis);
-		animTo.setAnimationListener(new AnimationListenerAdapter() {
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				setInvisible(true, visibleViewFrom);
-				setInvisible(false, invisibleViewTo);
-				if (onAnimationEnd != null) {
-					onAnimationEnd.run();
-				}
-			}
-		});
-		visibleViewFrom.startAnimation(animFrom);
-		invisibleViewTo.startAnimation(animTo);
+	public static void putCursorAfterLastSymbol(EditText editText) {
+		editText.setSelection(editText.getText().length());
 	}
 
 	public static void showKeyboard(final View view) {
@@ -85,23 +60,6 @@ public class ViewUtils {
 			imm.showSoftInput(view, 0);
 		} else {
 			imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-		}
-	}
-
-	public static void putCursorAfterLastSymbol(EditText editText) {
-		editText.setSelection(editText.getText().length());
-	}
-
-	public static void disableOverscroll(View view) {
-		Class<?> viewCls = view.getClass();
-		try {
-			Method m = viewCls.getMethod("setOverScrollMode",
-					new Class[] { int.class });
-			int OVER_SCROLL_NEVER = (Integer) viewCls.getField(
-					"OVER_SCROLL_NEVER").get(view);
-			m.invoke(view, OVER_SCROLL_NEVER);
-		} catch (Exception e) {
-			L.w(e);
 		}
 	}
 
