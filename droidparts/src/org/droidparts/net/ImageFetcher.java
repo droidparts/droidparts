77ea6b6caa86e2cbab53ce90e35c9f9e2bb0f8f1
Alex Yanchenko <alex@yanchenko.com>
2013-03-27 16:35:53 +0200

    ImageFetcher: back to 2 threads by default.

--- droidparts/src/org/droidparts/net/ImageFetcher.java
+++ droidparts/src/org/droidparts/net/ImageFetcher.java
@@ -61,7 +61,7 @@ public class ImageFetcher {
 	int crossFadeMillis = 0;
 
 	public ImageFetcher(Context ctx) {
-		this(ctx, (ThreadPoolExecutor) Executors.newFixedThreadPool(1),
+		this(ctx, (ThreadPoolExecutor) Executors.newFixedThreadPool(2),
 				new RESTClient(ctx), BitmapMemoryCache.getDefaultInstance(ctx),
 				BitmapDiskCache.getDefaultInstance(ctx));
 	}
