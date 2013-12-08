f8e9fe293c4a7fb5d73176c3104318b208a3bc94
makovkastar <makovkastar@gmail.com>
2013-12-08 21:54:44 +0200

    Add POST multipart file.

--- droidparts-test/src/org/droidparts/test/testcase/RESTClientTestCase.java
+++ droidparts-test/src/org/droidparts/test/testcase/RESTClientTestCase.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Alex Yanchenko
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,6 +14,13 @@
  * limitations under the License. 
  */
 package org.droidparts.test.testcase;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.droidparts.net.http.HTTPException;
 import org.droidparts.net.http.HTTPResponse;
@@ -27,6 +34,11 @@ public class RESTClientTestCase extends AndroidTestCase {
 
 	private static final String AUTH_URL = "http://www.httpwatch.com/httpgallery/authentication/authenticatedimage/default.aspx";
 	private static final String AUTH_LOGIN = "httpwatch";
+	// Simple server for testing POST requests. Supports multipart/form-data
+	// file uploads.
+	private static final String POST_MULTIPART_URL = "http://posttestserver.com/post.php?dump";
+	private static final String POST_MULTIPART_FILE_NAME = "test";
+	private static final String POST_MULTIPART_FILE_BODY = "Test POST multipart file";
 
 	public void _testHttpBasicAuth() {
 		RESTClient2 client = new RESTClient2(getContext());
@@ -41,6 +53,42 @@ public class RESTClientTestCase extends AndroidTestCase {
 		testUnauthenticated(client);
 		testAuthenticatedWrongCredentials(client);
 		testAuthenticated(client);
+	}
+
+	public void testPostMultipartFile() throws Exception {
+		RESTClient client = new RESTClient(getContext());
+		HTTPResponse resp = client.postMultipartFile(POST_MULTIPART_URL,
+				POST_MULTIPART_FILE_NAME, getTestFile());
+		assertPostMultipartFileResponse(resp);
+	}
+
+	public void testPostMultipartFileLegacy() throws Exception {
+		RESTClient client = new RESTClient(getContext(), new HttpClientWorker(
+				RESTClient.getUserAgent(null)));
+		HTTPResponse resp = client.postMultipartFile(POST_MULTIPART_URL,
+				POST_MULTIPART_FILE_NAME, getTestFile());
+		assertPostMultipartFileResponse(resp);
+	}
+
+	private void assertPostMultipartFileResponse(HTTPResponse response)
+			throws HTTPException {
+		assertNotNull(response);
+		String body = response.body;
+
+		// Get uploaded file URL
+		Pattern pattern = Pattern.compile("Uploaded File: (.+)");
+		Matcher matcher = pattern.matcher(body);
+		assertTrue(matcher.find());
+
+		String fileUrl = matcher.group(1);
+		RESTClient2 client = new RESTClient2(getContext());
+		assertEquals(client.get(fileUrl).body, POST_MULTIPART_FILE_BODY);
+	}
+
+	private File getTestFile() throws IOException {
+		File file = new File(getContext().getFilesDir(), "test.txt");
+		writeFile(file, POST_MULTIPART_FILE_BODY);
+		return file;
 	}
 
 	private void testUnauthenticated(RESTClient client) {
@@ -72,4 +120,10 @@ public class RESTClientTestCase extends AndroidTestCase {
 		}
 	}
 
+	private void writeFile(File file, String string) throws IOException {
+		file.getParentFile().mkdirs();
+		DataOutputStream dout = new DataOutputStream(new FileOutputStream(file));
+		dout.write(string.getBytes());
+		dout.close();
+	}
 }
--- droidparts/src/org/droidparts/net/http/RESTClient.java
+++ droidparts/src/org/droidparts/net/http/RESTClient.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Alex Yanchenko
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,8 @@
  */
 package org.droidparts.net.http;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.Date;
 
@@ -148,6 +150,27 @@ public class RESTClient {
 			response = httpClientWorker.getResponse(req, true);
 		}
 		L.d(response);
+		return response;
+	}
+
+	public HTTPResponse postMultipartFile(String uri, String name, File file)
+			throws IOException, HTTPException {
+		if (file == null || !file.exists()) {
+			throw new IllegalArgumentException(
+					"'file' must not be null and must exist");
+		}
+		L.i("POST on '%s', file: '%s' .", uri, file.getPath());
+		HTTPResponse response;
+		if (httpURLConnectionWorker != null) {
+			HttpURLConnection conn = httpURLConnectionWorker.getConnection(uri,
+					Method.POST);
+			HttpURLConnectionWorker.postMultipartFile(conn, name, file);
+			response = HttpURLConnectionWorker.getResponse(conn, true);
+		} else {
+			HttpPost req = new HttpPost(uri);
+			req.setEntity(HttpClientWorker.buildMultipartEntity(name, file));
+			response = httpClientWorker.getResponse(req, true);
+		}
 		return response;
 	}
 
